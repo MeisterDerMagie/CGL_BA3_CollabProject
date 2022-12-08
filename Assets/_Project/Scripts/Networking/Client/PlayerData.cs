f1d957d86a5968399f93aae63f784500d7cbad29
Martin Work <martin.kloeckener@gmail.com>
2022-12-08 16:37:36 +0100

    test chat

--- Assets/_Project/Scripts/Networking/Client/PlayerData.cs
+++ Assets/_Project/Scripts/Networking/Client/PlayerData.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using MEC;
+using Sirenix.OdinInspector;
 using Unity.Netcode;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -9,15 +10,22 @@ using Random = UnityEngine.Random;
 public class PlayerData : NetworkBehaviour
 {
     [SerializeField] private NetworkVariable<int> testInt = new(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    [SerializeField] private NetworkVariable<string> testString = new(string.Empty, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
     private CoroutineHandle coroutine;
     
     #if !UNITY_SERVER
     public override void OnNetworkSpawn()
     {
         Debug.Log($"OnNetworkSpawn: {OwnerClientId}");
-        
-        
+
+        testString.OnValueChanged += OnTestStringChanged;
         if(IsLocalPlayer) coroutine = Timing.RunCoroutine(_NewInt());
+    }
+
+    private void OnTestStringChanged(string previousvalue, string newvalue)
+    {
+        Debug.Log($"Player {OwnerClientId.ToString()} said: {newvalue}");
     }
 
     private IEnumerator<float> _NewInt()
@@ -33,5 +41,8 @@ public class PlayerData : NetworkBehaviour
     {
         if (coroutine.IsRunning) Timing.KillCoroutines(coroutine);
     }
+
+    [Button]
+    private void SaySomething(string text) => testString.Value = text;
     #endif
 }
