bf6522a59a4bf0822f330f4ecfa5c8000c23a3f4
elfenbeinstein <96117493+elfenbeinstein@users.noreply.github.com>
2022-12-13 18:10:04 +0100

    beat lines functional

--- Assets/_Project/Scripts/PianoRoll/Notes.cs
+++ Assets/_Project/Scripts/PianoRoll/Notes.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class Notes : MonoBehaviour
 {
-    int bpm;
     float targetX;
 
 
-    void Update()
+    public void NoteSetUp(float bpm, int beatLength, float _targetX)
     {
-        
+        targetX = _targetX;
+
+        // total duration of travelling length of the piano roll
+        // quarter note = bpm/60/4
+        // beat length = length of piano roll measured in quarter notes
+        float duration = bpm / 60 / 4 * beatLength;
+
+        StartCoroutine(MoveToLeft(duration));
     }
 
-    public void NoteSetUp(int bpm, int beatLength, float xLength)
+    IEnumerator MoveToLeft(float duration)
     {
+        float time = 0;
+        Vector3 startPosition = transform.position;
+        Vector3 targetPosition = new Vector3(targetX, transform.position.y, transform.position.z);
 
+        while (time < duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
+        Destroy(this.gameObject);
     }
 }
--- Assets/_Project/Scripts/PianoRoll/PianoRoll.cs
+++ Assets/_Project/Scripts/PianoRoll/PianoRoll.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PianoRoll : MonoBehaviour
 {
-    [SerializeField] private int bpm = 110;
+    [SerializeField] private float bpm = 110f;
     [Tooltip("Amount of lines in piano roll, so how many different kinds of notes spawned on top of each other")]
     [SerializeField] private int lines = 4;
     [Tooltip("Length of Piano Roll in beats, how many beats fit into piano roll")]
@@ -14,31 +14,84 @@ public class PianoRoll : MonoBehaviour
     [SerializeField] private GameObject noteObj;
     [SerializeField] private GameObject bg;
 
+    float xPos; // where new notes should be spawned
+    float length4s; // length of the quarter notes
     float timer;
-    float xPos;
+    int beatCounter;
+    bool playing;
 
     // Start is called before the first frame update
     void Start()
     {
         // get the right corner of the piano roll (where objects will be spawned)
         xPos = transform.position.x + bg.transform.localScale.x / 2;
+        length4s = bpm / 60 / 4;
+        timer = 0;
+        beatCounter = 0;
     }
 
     void Update()
     {
-        // for testing the spawn
-        if (Input.GetKey(KeyCode.Space))
+        // for testing if quarter lines spawn correctly on time:
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // way 1:
+            //StartCoroutine(SpawnQuarterTicks());
+            playing = !playing;
+        }
+
+        // way 2
+        //return;
+        if (playing) PlayQuarterNotes();
+        else
+        {
+            timer = 0; 
+            beatCounter = 0;
+        }
+    }
+
+    void PlayQuarterNotes()
+    {
+        if (beatCounter == 0)
+        {
+            SpawnQuarterLine();
+            beatCounter++;
+        }
+        else
         {
             timer += Time.deltaTime;
 
-            // spawn Viertel:
-            if (timer >= bpm/60)
+            if (timer >= bpm / 60 / 4 * beatCounter)
             {
-                GameObject clone = Instantiate(beatObj, this.gameObject.transform);
-                clone.transform.position = new Vector3(xPos, 0, 0);
-                clone.GetComponent<Notes>().NoteSetUp(bpm, beats, bg.transform.localScale.x);
+                SpawnQuarterLine();
+                beatCounter++;
             }
         }
-        else timer = 0;
+    }
+
+    IEnumerator SpawnQuarterTicks()
+    {
+        SpawnQuarterLine();
+        for (int i = 0; i < 3; i++)
+        {
+            yield return new WaitForSeconds(length4s);
+            SpawnQuarterLine();
+        }
+    }
+
+    void SpawnQuarterLine()
+    {
+        GameObject clone = Instantiate(beatObj, this.gameObject.transform);
+        clone.transform.position = new Vector3(xPos, 0, 0);
+        clone.GetComponent<Notes>().NoteSetUp(bpm, beats, transform.position.x - bg.transform.localScale.x / 2);
+        clone.SetActive(true);
+    }
+
+    void SpawnNote()
+    {
+        GameObject clone = Instantiate(noteObj, this.gameObject.transform);
+        // missing: set correct y Pos according to line
+        clone.transform.position = new Vector3(xPos, 0, 0);
+        clone.GetComponent<Notes>().NoteSetUp(bpm, beats, transform.position.x - bg.transform.localScale.x / 2);
     }
 }
