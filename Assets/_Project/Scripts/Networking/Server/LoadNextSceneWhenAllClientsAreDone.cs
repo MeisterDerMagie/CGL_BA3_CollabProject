5ef9b047aa58353115c7cb0fc729d8808a50b7f1
Martin Work <martin.kloeckener@gmail.com>
2023-01-04 14:19:24 +0100

    scores scene

---
+++ Assets/_Project/Scripts/Networking/Server/LoadNextSceneWhenAllClientsAreDone.cs
@@ -0,0 +1,81 @@
+﻿//(c) copyright by Martin M. Klöckener
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextSceneWhenAllClientsAreDone : NetworkBehaviour
+{
+    [SerializeField] private NetworkSceneLoader nextScene;
+    
+    private Dictionary<ulong, bool> _clientStates = new Dictionary<ulong, bool>();
+
+    public override void OnNetworkSpawn()
+    {
+        //only run on server
+        if (!NetworkManager.IsServer) return;
+        
+        //add all clients to the dictionary
+        foreach (ulong clientId in NetworkManager.ConnectedClientsIds)
+        {
+            _clientStates.Add(clientId, false);
+        }
+        
+        //subscribe to client disconnect event
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnected;
+    }
+
+    //call this on the client when they are done with the current scene
+    public void Done()
+    {
+        if (NetworkManager.IsServer)
+        {
+            Debug.LogWarning("Don't call this method on the server.", this);
+            return;
+        }
+        
+        DoneServerRpc(NetworkManager.LocalClientId);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void DoneServerRpc(ulong clientId)
+    {
+        SetClientDone(clientId);
+    }
+
+    private void OnClientDisconnected(ulong clientId)
+    {
+        //treat disconnected clients as done
+        SetClientDone(clientId);
+    }
+
+    private void SetClientDone(ulong clientId)
+    {
+        //set client state
+        if (_clientStates.ContainsKey(clientId))
+            _clientStates[clientId] = true;
+        
+        //if all clients are done, load next scene
+        bool allClientsDone = true;
+
+        foreach (KeyValuePair<ulong, bool> clientState in _clientStates)
+        {
+            if (clientState.Value == false)
+            {
+                allClientsDone = false;
+                break;
+            }
+        }
+        
+        if(allClientsDone)
+            LoadNextScene();
+
+        Debug.LogWarning("WATCH OUT! HERE COULD BE A PROBLEM WITH ONCLIENTDISCONNECT. DOES IT GET CALLED EVEN AFTER THE NEW SCENE WAS LOADED?");
+    }
+
+    private void LoadNextScene()
+    {
+        NetworkSceneLoading.LoadNetworkScene(nextScene, LoadSceneMode.Single);
+    }
+}
---
+++ Assets/_Project/Scripts/Networking/Server/WaitingScreenController_Generic.cs
@@ -0,0 +1,32 @@
+﻿//(c) copyright by Martin M. Klöckener
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class WaitingScreenController_Generic : WaitingScreenController
+{
+    //call this on the client if they are ready
+    public void Ready()
+    {
+        ReadyServerRpc(NetworkManager.LocalClientId);
+    }
+    
+    //call this along with Ready()
+    public override void Show()
+    {
+        transform.localScale = Vector3.one;
+    }
+
+    private void Start()
+    {
+        transform.localScale = Vector3.zero;
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void ReadyServerRpc(ulong clientId)
+    {
+        SetDoneState(clientId, true);
+    }
+}
