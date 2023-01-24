bc47f71a167b712bcd8cfa8e1a64dfef35e4b1d3
Martin Work <martin.kloeckener@gmail.com>
2023-01-24 17:56:12 +0100

    playerdata

--- Assets/_Project/Scripts/Networking/Client/PlayerData.cs
+++ Assets/_Project/Scripts/Networking/Client/PlayerData.cs
@@ -402,8 +402,18 @@ public class PlayerData : NetworkBehaviour
     
     //The points should not have a ServerRPC (the client should not have the authority to set its points). The server should calculate those based on the user input
     public void AddPointsCreativity(int valueToAdd) => _pointsCreativity.Value += valueToAdd;
+
     public void AddPointsPlayability(int valueToAdd) => _pointsPlayability.Value += valueToAdd;
     public void AddPointsPerformance(int valueToAdd) => _pointsPerformance.Value += valueToAdd;
+    public void AddPointsPlayabilityPercent(float accuracyPercent)
+    {
+        
+    }
+
+    public void AddPointsPerformancePercent(float accuracyPercent)
+    {
+        
+    }
     
     #endregion
     
