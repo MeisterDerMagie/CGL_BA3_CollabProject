48d7c972c547e1ae455a06ca75644b6fe81496ef
Martin Work <martin.kloeckener@gmail.com>
2023-01-25 14:58:15 +0100

    playability

---
+++ Assets/_Project/Scripts/RecAndPlayback/Kofferphase/PlayabilityCalculation.cs
@@ -0,0 +1,15 @@
+﻿//(c) copyright by Martin M. Klöckener
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayabilityCalculation : MonoBehaviour
+{
+    [ServerRpc(RequireOwnership = false)]
+    public void SubmitPlayabilityServerRpc(Guid performingPlayer, Guid ratedPlayer, float percent)
+    {
+        
+    }
+}
