2fac1535aed4f1bdff8585215392eccbecade779
Martin Work <martin.kloeckener@gmail.com>
2023-01-25 14:59:06 +0100

    fix

--- Assets/_Project/Scripts/RecAndPlayback/Kofferphase/PlayabilityCalculation.cs
+++ Assets/_Project/Scripts/RecAndPlayback/Kofferphase/PlayabilityCalculation.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class PlayabilityCalculation : MonoBehaviour
+public class PlayabilityCalculation : NetworkBehaviour
 {
     [ServerRpc(RequireOwnership = false)]
     public void SubmitPlayabilityServerRpc(Guid performingPlayer, Guid ratedPlayer, float percent)
